622060669b0f564142bcfc71bb153080f0adef1c
ChaosCoder25 <mwill908@evit.edu>
2024-08-16 12:38:14 -0700

    exported from Skill Struck

---
+++ intro/ syntax and comments.py
@@ -0,0 +1,12 @@
+print("my farovite movie is mha")
+print("my favorite movie is trolls")
+print("my farvorite movie is dbz broly")
+#Mha is a great movie cause its fights
+#trolls sing amazing song i remember
+#DBZ broly is great beauase of the story 
+
+
+
+
+
+
