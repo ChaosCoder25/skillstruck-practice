5b0ef72b4ab52453c41f14661a9a299748d07309
ChaosCoder25 <mwill908@evit.edu>
2024-08-29 13:39:15 -0700

    exported from Skill Struck

---
+++ intro/concatenation.py
@@ -0,0 +1,9 @@
+animal = input("what animal")
+place = input("what place")
+noun = input("what noun")
+adjective = input("what adjective")
+
+
+
+
+print("Today I rode a " + animal + " to the " + place + " to buy some " + noun + ". It was " + adjective + ".")
