3ad7b1983039eb31ae8e17de741700f2fdaa2eca
ChaosCoder25 <mwill908@evit.edu>
2024-08-29 13:39:18 -0700

    exported from Skill Struck

---
+++ intro/string methods.py
@@ -0,0 +1,26 @@
+#string1 = "I love Coding!"
+#print(string1.lower())
+#print(string1.upper())
+
+
+
+# string1 = "Coding is awsome!"
+# print(string1.replace("awsome", "fun"))
+
+
+#vacation = "I like going to the beach!"
+
+#print(vacation.replace("beach", "mountain"))
+
+
+
+
+
+#string1 =input("how are doing today?")
+#print(string1.upper())
+
+string1 = input("test@test.com")
+print(string1.replace("@",""))
+
+
+
