a356a82866f8a44560d4f7bb3a3b84d5230e5589
ChaosCoder25 <mwill908@evit.edu>
2024-08-29 13:39:18 -0700

    exported from Skill Struck

---
+++ intro/strings methods continued.py
@@ -0,0 +1,24 @@
+# test = "I love Coding!"
+# reverse = test[len(test)::-1]
+# print(reverse)
+
+# string1 ="I love sushi!"
+
+# print(string1.split())
+
+
+# string1 = "I Hate BellaTheWolf!"
+
+# print(len(string1))
+
+# string1 = "I love Panda Express!"
+
+# print(string1.find("a"))
+
+# string1 = "packgod roasts bella"
+# print(strin    
+word = input("input a phrase")
+
+first = word.find("e")
+last = word.rfind("e")
+print(str(first) + "-" + str(last))
