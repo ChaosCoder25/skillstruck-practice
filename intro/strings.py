8caacd03c2baba3ddca11dad00eb25b49569c4d9
ChaosCoder25 <mwill908@evit.edu>
2024-08-29 13:39:17 -0700

    exported from Skill Struck

---
+++ intro/strings.py
@@ -0,0 +1,27 @@
+# string = '''This is a multiline string I can use this,
+# so i can use this in the future for my coding
+# plus how are hope you are doing.'''
+
+
+
+
+# name1 = "malachi"
+# name2 = "eathen"
+# name3 = "finn"
+# name4 = "nova"
+# print(name1)
+# print(name2)
+# print(name3)
+# print(name4)
+
+short_quote ='This one is a short quote is is small.'
+
+
+
+long_quote ="""This is a long quote and my grandfather
+said to take control of your life take responsabilty
+never give up."""
+
+print(short_quote)
+print(long_quote)
+
