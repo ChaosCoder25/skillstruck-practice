b167390bf8c47dca0af94386a1b71a429e5d11f7
ChaosCoder25 <mwill908@evit.edu>
2024-09-06 14:27:22 -0700

    exported from Skill Struck

--- variables.py
+++ variables.py
@@ -1,3 +1,6 @@
-dessert = input("pie")
-
-print("dessert")
+import pygame
+window = pygame.display.set_mode([400,300])
+pygame.display.set_caption("Images")
+image = pygame.image.load("imagel.png")
+windoe.blit(image, [0,0])
+pygame.display.flip()
