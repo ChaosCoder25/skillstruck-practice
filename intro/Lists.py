19bdddcfa2c0dd75d67d44f5ccfc7470bf668c22
ChaosCoder25 <mwill908@evit.edu>
2024-09-06 14:27:28 -0700

    exported from Skill Struck

---
+++ intro/Lists.py
@@ -0,0 +1,10 @@
+# Get user input
+number = float(input("Enter a number: "))
+
+# Check if the number is positive or negative
+if number > 0:
+    print("1")
+elif number < 0:
+    print("-1")
+else:
+    print("1")
