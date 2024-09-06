da2cbf743517b498e60eb2c3b857a51a6435fe1d
ChaosCoder25 <mwill908@evit.edu>
2024-09-06 14:27:30 -0700

    exported from Skill Struck

---
+++ intro/python else if statements.py
@@ -0,0 +1,14 @@
+vacation = "mountains"
+
+if vacation =="beach":
+    print("you love the ocean")
+elif vacation == "amusemrnt_park":
+    print("you like to ride roller coasters")
+elif vacation == "mountains":
+        print("you love to get up and away")
+else:
+    print("you like unique vacation destinations")
+
+
+
+
