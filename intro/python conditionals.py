4ae11bd5fb6f66d9c8520e6ef28e2d2ca94d4588
ChaosCoder25 <mwill908@evit.edu>
2024-09-06 14:27:29 -0700

    exported from Skill Struck

---
+++ intro/python conditionals.py
@@ -0,0 +1,53 @@
+# your_pet = "gerbil"
+
+
+# if your_pet == "dog" and your_need == "reptile":
+#     print("your are allowed to brind your pet inside the store.")
+# else:
+#        print("your cannot bring your pet inside the store.")
+# friend_pet = "poodle"
+# your_pet = "fish"
+
+# if friend_pet == your_pet:
+#     print("You and your friend have the same pet!")
+# else:
+#     print("You and your friend have different pets.")
+# friend_pet = "poodle"
+# your_pet = "fish"
+
+# if friend_pet != your_pet:
+#      print("You and your friend have the same pet!")
+# else:
+#      print("You and your friend have different pets.")
+#height_in_inches = int(input(""))
+#if height_in_inches >= 50:
+#     print("You can ride the roller coaster.")
+#else:
+#     print("You can't ride the roller coaster.")
+
+# age = int(input(""))
+# has_license = True
+# if age >= 16 and has_license ==True:
+#     print("your able to drive")
+# else:
+#    print("you are unable to drive")
+# candies = 100
+
+# if candies >= 100:
+#     print("You have enough candy for the party!")
+# else:
+#     print("You need to get more candy.")
+vacation = "mountains"
+
+if vacation =="beach":
+    print("you love the ocean")
+elif vacation == "amusemrnt_park":
+    print("you like to ride roller coasters")
+elif vacation == "mountains":
+        print("you love to get up and away")
+else:
+    print("you like unique vacation destinations")
+
+
+
+
