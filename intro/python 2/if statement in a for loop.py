3850b77b5d223cb15316480591fa3a3a180138dd
ChaosCoder25 <mwill908@evit.edu>
2024-09-13 12:12:32 -0700

    exported from Skill Struck

---
+++ intro/python 2/if statement in a for loop.py
@@ -0,0 +1,27 @@
+# ages = [14, 15 , 16 ,17 , 18 , 19 , 10]
+
+# for x in ages:
+#     if x >= 16:
+#         print("he is old enough to drive")
+#     else:
+#         print("he is not old enough to drive")
+
+# smells = ["shunk", "lilac", "rain", "ocean", " garbage", "cleaner", "cookies"]
+
+# print(smells[2:5])
+
+# my_list =[2, 5, 8, 10]
+
+# total = 0
+# for x in my_list:
+#     total = total + x
+
+# print(total)
+# my_list = [int(x) for x in input("").split()]
+# for x in my_list:
+#     if x % 2 ==0 :
+#         print(x)
+my_list = [int(x) for x in input("Input a list of numbers").split()]
+for x in my_list :
+if x  ==0:
+				print(my_list)
