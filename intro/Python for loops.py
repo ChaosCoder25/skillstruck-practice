bcbf1af2c6652746156e4b287c41ca9d46dad41c
ChaosCoder25 <mwill908@evit.edu>
2024-09-13 12:12:31 -0700

    exported from Skill Struck

---
+++ intro/Python for loops.py
@@ -0,0 +1,31 @@
+# animals = ["hedgehogs", "porcupines","rats","mice"]
+# for x in animals:
+#     print(x + "is the spikest animal ever!")
+# sweets = ["cotton candy", "doughnuts", "chocolete bars"]
+# for x in sweets:
+#     print("I love to eat" + x)
+# allowance = [8,5,10,4,7]
+# for x in allowance:
+#     print(x + 10)
+# for x in range(2, 10 + 1):
+#     print(x)
+# total = 0
+# for x in range(4):
+#     total = total + x
+
+#     print(total)
+#sports = ["soccer", "basketball", "volleyball"]
+
+#for x in sports:
+#    print("I love to play " + x)
+# first = int(input(""))
+# second = int(input(""))
+# total = 0
+# for x in range(first,second):
+    
+#     total = total + x
+   
+
+print(total)
+   
+      
