7e36d2b8e2c440bf76c7c354d94caa29c40b1c9a
ChaosCoder25 <mwill908@evit.edu>
2024-11-01 12:08:58 -0700

    exported from Skill Struck

---
+++ intro/python 2/python_functions.py
@@ -0,0 +1,39 @@
+# def weather():
+#     print("Its's a soggy day outside!")
+
+
+#     weather()
+# favorite = "I love juice"
+# def myfunction():
+#     global fruit
+#     fruit = "apple"
+# myfunction()
+
+# print(favorite)
+# print(fruit)
+# Define the function
+# def charlie():
+#     print("Charlie is a great dog!")
+    
+
+# charlie()
+# length = 5
+# def surface_area():
+#     print(length * 6)
+    
+
+# surface_area()
+
+# def volume():
+#     print(length * length * length)
+    
+
+# volume()
+def race():
+    print("Tommy has won the race")
+
+
+    
+    
+    
+race()
