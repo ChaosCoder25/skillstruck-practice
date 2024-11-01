653bb80b2c7219c93950abcb67ae910ade11ed6c
ChaosCoder25 <mwill908@evit.edu>
2024-11-01 12:09:01 -0700

    exported from Skill Struck

---
+++ intro/python 2/adding_to_and_removing_from_dictionaries.py
@@ -0,0 +1,58 @@
+# classmates = {
+#     "Allen": 15,
+#     "Alex": 16,
+#     "Eliot": 13,
+# }
+
+# classmates["Alex"] = 17
+
+# print(classmates)
+# friends = {}
+
+# friends["Allen"] =  16
+# friends["Alex"] = 17
+
+# print(friends)
+# classmates = {
+#      "Allen": 15,
+#      "Alex": 16,
+#      "Eliot": 13,
+# }
+
+# classmates.pop("Alex")
+
+# print(classmates)
+# friends = {
+# 	"Shane" : 10,
+# 	"Samantha" : 9,
+# 	"Shiloh" : 12,
+# 	"Sean" : 11	
+# }
+
+# friends["Sebastian"] = 8
+# print(friends)
+# friends.pop("Shiloh")
+# print(friends)
+# name = input("Which tree do you want to remove?")
+# trees = {
+# 	"aspen" : 75,
+# 	"pine" : 82,
+# 	"maple" : 60,
+# 	"oak" : 65,
+# 	"willow" : 80,
+# 	"cottonwood" : 100,	
+# }
+
+# trees.pop(name)
+# print(trees)
+plan = input("Which goal do you want to remove?")
+goals = {
+	"piano" : 5,
+	"run" : 3,
+	"paint" : 2,
+	"serve" : 7,
+	"homework" : 7,
+}
+
+goals.pop(plan)
+print(goals)
