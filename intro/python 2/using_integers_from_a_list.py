e3b6f6b43c02d738a711f2b58dc2bae6ce40bee0
ChaosCoder25 <mwill908@evit.edu>
2024-11-01 12:08:54 -0700

    exported from Skill Struck

---
+++ intro/python 2/using_integers_from_a_list.py
@@ -0,0 +1,40 @@
+# unlucky_numbers =[4, 13, 24, 66, 44]
+
+# print(unlucky_numbers[0] + 10)
+
+# pies = 10
+# shared = pies/3
+# print(round(shared, 2))
+# for x in range(0,5):
+#     print(x)
+
+#smells = ["skunk", "lilac", "rain", "ocean", "garbage", "cleaner", "cookies", "roses", "new car",  "sweaty", "feet", "peach",]
+
+#print(smells[0:8:2])
+# for x in range(0, 20, 3):
+#     print(x)
+
+#largest = max(4, 20, 62, 18, 9, 45)
+
+#print(largest)
+
+# smallest = min(4, 20, 62, 18, 9, 45)
+# print(smallest)
+# lasers = [3, 10, 4, 15, 11]
+# print(lasers[2] * 10)
+# for x in range(0,10):
+#     print(x)
+# for x in range(0,10,2):
+#     print(x)
+
+# my_list = [int(n) for n in input().split()]
+
+# sum1 = 0
+# for x in my_list:
+#     sum1 = sum1 + x
+# print(sum1)
+my_list = [int(n) for n in input().split()]
+product1 = 1
+for x in my_list:
+    product1 = product1 * x
+print(product1)
