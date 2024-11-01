c0869bf77e556af0d0f5e8f80411d26588da69b7
ChaosCoder25 <mwill908@evit.edu>
2024-11-01 12:08:59 -0700

    exported from Skill Struck

---
+++ intro/python 2/python_functions_parameters.py
@@ -0,0 +1,33 @@
+# def weather(forecast):
+#      print("It's a soggy day outside because it is " + forecast)
+
+
+# weather("raining")
+# weather("snowing")
+# weather("drizzling")
+# def practice(activity):
+# 	print("I like to practice " + activity)
+
+# practice("piano")
+# def practice(skill):
+#   print("Today I will practice " + skill)
+#   print("I enjoy practicing " + skill)
+
+
+# practice("piano")
+# practice("soccer")
+# practice("karate")
+# answer = int(input("What is the radius of the circle?"))
+
+# def circle (radius) :
+# 	answer = 3.14159 * radius * radius
+# 	print(round(answer,1 ))
+	
+
+# circle(answer)
+# fruit = input("Please enter the name of a fruit: ")
+
+# def hungryForApples(choice):
+#     print( fruit + fruit + fruit )
+	
+# hungryForApples(fruit)
