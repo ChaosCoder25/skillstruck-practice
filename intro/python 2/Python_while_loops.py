f1574fc9d853ff75d9b4ec92f5be571da95e7bb1
ChaosCoder25 <mwill908@evit.edu>
2024-11-01 12:08:57 -0700

    exported from Skill Struck

---
+++ intro/python 2/Python_while_loops.py
@@ -0,0 +1,39 @@
+# people_in_line = 20
+
+# while people_in_line > 0:
+#     print("There are" + str(people_in_line) + " people in linr. Keep the coaster running!")
+#     people_in_line = people_in_line - 1
+# people_in_line = 20
+
+# while people_in_line > 0:
+#     print("There are" + str(people_in_line) + " people in linr. Keep the coaster running!")
+#     people_in_line -= 1
+# age = 17
+# while age >= 0:
+#     print(age)
+#     age -= 1
+# age = 17
+# while age >= 0:
+#     if age == 5 or age == 10:
+#         print(age)
+#     age -= 1
+# number = 1
+# while number <= 50:
+# 	print(number)
+# 	number += 1
+
+# my_number = int(input("Pick a number."))
+# total = my_number
+# while my_number != 0:
+# 	my_number = int(input("Pick a number."))
+# 	total += my_number
+	
+# print(total)
+my_number = int(input("Pick a number."))
+largest = my_number
+while my_number != 0:
+	my_number = int(input("Pick a number."))
+	if my_number > largest :
+		largest = my_number
+
+print(largest)
