54cdf211c4c277a469f67cda5923723cc13b23c7
ChaosCoder25 <mwill908@evit.edu>
2024-11-01 12:08:56 -0700

    exported from Skill Struck

---
+++ intro/python 2/removing_items_from_a_list.py
@@ -0,0 +1,31 @@
+#goats = ["billy","frenchie","leslie","barbara"]
+
+#goats.remove("billy")
+
+#print(goats)
+#goats = ["billy","frenchie","leslie","barbara", "bob", "scott"]
+
+#goats.remove("bob")
+
+#print(goats)
+# goats = ["billy","frenchie","leslie","barbara", "bob", "scott"]
+
+# favorite = goats.pop(3)
+# print(favorite + " is my favorite goat")
+
+# print(goats)
+# harvest = ["pumpkins", "apples", "corn", "squash", "carrots"]
+# harvest.remove("squash")
+# favorite = harvest.pop(1)
+# print(harvest)
+# choice = input("What skill do you want to remove?")
+
+# skills = ["Piano", "Soccer", "Coding", "Cooking", "Writing"]
+
+# skills.remove(choice)
+
+# print(skills)
+#my_list = input().split()
+
+#print(my_list.pop(2) + " got the bronze medal")
+# Create a list of integers from the input
