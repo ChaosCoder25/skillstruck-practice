6dca650156c015edfbe7dd8beed318cf9a8c137c
ChaosCoder25 <mwill908@evit.edu>
2024-11-01 12:08:53 -0700

    exported from Skill Struck

--- intro/python 2/if statement in a for loop.py
+++ intro/python 2/if statement in a for loop.py
@@ -21,7 +21,31 @@
 # for x in my_list:
 #     if x % 2 ==0 :
 #         print(x)
-my_list = [int(x) for x in input("Input a list of numbers").split()]
-for x in my_list :
-if x  ==0:
-				print(my_list)
+
+# choice = input("")
+
+# my_list = ["red", "orange", "yellow", "green", "blue", "purple", "black", "white", "gray", "pink", "indigo", "brown", "tan", "gold", "silver"]
+
+# total = 0
+
+# for x in my_list:
+#     if choice in x:
+#         total += 1
+    
+# print( "There are "  + str(total) + " items in the list that have the letter " + choice +  " in it.")
+
+foods = ["mushrooms", "broccoli", "fish", "liver", "brussels sprouts"]
+
+
+for x in foods:    
+    if x == "mushrooms":
+         print(x + " are ross")
+    
+    elif x == "broccoli":
+         print(x + " is gros")
+     
+    elif x == "fish":
+         print(x + " is gros")
+    
+    else:
+        print(x + " are also gross")
