250d3bf6d87510a68b9d42ae6debcbf1de04dc7b
ChaosCoder25 <mwill908@evit.edu>
2024-11-01 12:09:00 -0700

    exported from Skill Struck

---
+++ intro/python 2/Python_dictionaries.py
@@ -0,0 +1,39 @@
+# classmates = {
+#     "billy" : 8,
+#     "vance" : 12,
+#     "alice" : 10
+# }
+
+# print(classmates)
+# names = {
+#     "person1": "Georage",
+#     "person2": "Brenda",
+#     "person3": "Brendan",
+#     "person4": "Willy",
+# }
+
+# print(names)
+# classmates = {
+#      "billy" : 8,
+#      "vance" : 12,
+#      "alice" : 10
+# }
+
+# print(classmates["vance"])
+# classmates = {
+#      "billy" : 8,
+#      "vance" : 12,
+#      "alice" : 10
+# }
+
+# classmates["alice"] = 15
+
+# print(classmates)
+# mountains = {"Timpanogos" : 5, "Everest": 10, "Kilimanjaro": 6, "Vesuvius": 7}
+
+# print(mountains)
+# print(mountains["Vesuvius"])
+
+# mountains["Kilimanjaro"] = 15
+
+# print(mountains)
