fb262b29cab5abbf0eb0474fc97cd1178c7c06fb
ChaosCoder25 <mwill908@evit.edu>
2024-11-01 12:08:56 -0700

    exported from Skill Struck

---
+++ intro/python 2/python_list_continued.py
@@ -0,0 +1,49 @@
+#smells = ["skunk", "lilac", "rain", "ocean", "garbage","cleaner", "cookies"]
+
+#print(smells[2:5])
+# for x in range(3,5):
+#     print(x)
+# smells = ["skunk", "lilac", "rain", "ocean", "garbage","cleaner", "cookies"]
+
+# print(smells[0:6:2])
+# smells = ["skunk", "lilac", "rain", "ocean", "garbage","cleaner", "cookies"]
+
+# smells[3] = "perfume"
+
+# print(smells)
+# smells = ["skunk", "lilac", "rain", "ocean", "garbage","cleaner", "cookies"]
+
+# print(len(smells))
+# smells = ["skunk", "lilac", "rain", "ocean", "garbage","cleaner", "cookies"]
+# smells.sort()
+# print(smells)
+# cookies =[3, 55, 9, 23, 13]
+# cookies.sort()
+# print(cookies)
+# cookies =[3, 55, 9, 12, 13]
+# cookies.sort(reverse=True)
+# print(cookies)
+# activities = ["movies", "skating", "bowling", "laser tag", "escape room", "trampoline park", "library"]
+
+# print(activities[0:3])
+
+# activities[6] = "water park"
+
+# print(activities)
+
+my_list = [int(n) for n in input().split()]
+
+biggest = my_list[0]
+
+for x in my_list:
+    if x > biggest:
+        biggest = 
+print(my_list)
+# my_list = [int(n) for n in input().split()]
+
+# current = my_list[0]
+
+# for x in my_list:
+#     if x > current:
+#         print(x)
+#     current = x
