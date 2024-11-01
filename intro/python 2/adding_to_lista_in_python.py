b91446d038dad006a17064f41f218760b7f38928
ChaosCoder25 <mwill908@evit.edu>
2024-11-01 12:08:55 -0700

    exported from Skill Struck

---
+++ intro/python 2/adding_to_lista_in_python.py
@@ -0,0 +1,46 @@
+# goats = ["billy","frenchie","leslie","barbara"]
+
+# goats.append("ash")
+
+# print(goats)
+# goats = ["billy","frenchie","leslie","barbara"]
+
+# goats.extend(["ash", "annie", "ralph", "melodie"])
+
+# print(goats)
+# goats = ["billy","frenchie","leslie","barbara"]
+
+# goats.extend("curly")
+
+# print(goats)
+# goats = ["billy","frenchie","leslie","barbara"]
+
+# goats.insert(2,"curly ")
+
+# print(goats)
+# olympics = ["running", "gymnastics", "swimming", "volleyball", "basketball"]
+
+# new_olympics = ["karate", "surfing", "baseball", "skateboarding", "sport climbing"]
+
+# olympics.extend(["karate", "surfing", "baseball", "skateboarding", "sport climbing"])
+
+
+# print(olympics)
+
+
+
+ 
+
+
+
+spelling = []
+
+word = input("What word do you want spelled out?")
+
+spelling.append(word)
+
+spelling.extend(list(word))
+
+spelling.append(word)
+
+print(spelling)
