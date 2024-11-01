7c2974106223c13df51adf94cda514c9464ce755
ChaosCoder25 <mwill908@evit.edu>
2024-11-01 12:09:02 -0700

    exported from Skill Struck

---
+++ intro/python 2/looping_through_a_dictionary.py
@@ -0,0 +1,52 @@
+# classmates = {
+#     "Billy" : 8,
+#     "Vance" : 12,
+#     "Alice" : 10,
+#     "Eliza" : 15,
+#     "Xavier" : 6,
+# }
+
+# for x in classmates:
+#     print(x)
+# classmates = {
+#     "Billy" : 8,
+#     "Vance" : 12,
+#     "Alice" : 10,
+#     "Eliza" : 15,
+#     "Xavier" : 6,
+# }
+
+# for x in classmates.values():
+#     print(x)
+# classmates = {
+#     "Billy" : 8,
+#     "Vance" : 12,
+#     "Alice" : 10,
+#     "Eliza" : 15,
+#     "Xavier" : 6,
+# }
+
+# for x,y in classmates.items():
+#     print(x,y)
+# measurement = {"length": 10, "width": 5, "depth": 3}
+
+# for x in measurement.values():
+# 	print(x)
+ 
+amanda_value = int(input("How many in Amanda's group?"))
+jane_value = int(input("How many in Jane's group?"))
+
+group = {
+	"Fred" : 12,
+	"Jackson" : 15,
+	"Sophie" : 20,
+	"Amanda" : amanda_value,
+	"Jane" : jane_value,
+}
+
+total =  0
+
+for t in group.values():
+	total += amanda_value , jane_value 
+	
+print(t)
