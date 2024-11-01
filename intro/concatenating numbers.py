847b9bef5b8cf6b170db54a0fb445c0a8ca7d44a
ChaosCoder25 <mwill908@evit.edu>
2024-11-01 12:08:50 -0700

    exported from Skill Struck

--- intro/concatenating numbers.py
+++ intro/concatenating numbers.py
@@ -1,6 +1,6 @@
-cardio = "the time we  we've ran totals up to {} in {} minutes."
-miles = 23
-minutes = 30
-print(cardio.format,int(miles,minutes)) 
-
-
+ jasbags = int(input(""))
+cholebages = int(input(""))
+jasmarbles = jasbags * 12
+cholemarbles = cholebages * 10
+string1 = "If Jasmine had {} bags, she would have {} marbles total. If Chloe had {} bags, she would have {} marbles total."
+print(string1.format(jasbags,jasmarbles,cholebages,cholemarbles))
