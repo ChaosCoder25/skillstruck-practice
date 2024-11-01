b50d57f1cb50f8369d01e6fdd6008c801ecbaa3e
ChaosCoder25 <mwill908@evit.edu>
2024-11-01 12:08:53 -0700

    exported from Skill Struck

---
+++ intro/python 2/python_accessing_items_in_a_list.py
@@ -0,0 +1,34 @@
+
+# shapes =["square", "triangle", "circle", "rectangle" , "star"]
+# print(shapes[2])
+# print("I just drew a purple " + shapes[2] +  " on my paper")
+
+# lucky_list = ["quater", "penny", "rabbit foot", "socks"]
+
+# if "hats"not in lucky_list:
+#     print("hat is not in the lucky_list")
+# safari = "We saw so many animals!"
+# words = safari.split("o")
+# print(words)
+# animal = "giraffe"
+# mylist = list(animal)
+# print(mylist)
+# my_list = input().split()
+# my_list = [int(n) for n in input().split()]
+
+# my_list = [int(n) for n in input("Create a list of 9 numbers").split()]
+
+# my_list = [int(num) for num in my_list]
+
+
+# center = my_list[4]
+
+
+# print(center * 100)       
+
+# my_list = [int(n) for n in input("Create a list of 6 numbers").split()]
+
+# print(my_list[0] + my_list[1] + my_list[4] + my_list[5])
+
+
+
