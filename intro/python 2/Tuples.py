cadf8645537a2716481b6a79d7398559d7755e79
ChaosCoder25 <mwill908@evit.edu>
2024-11-08 13:10:42 -0700

    exported from Skill Struck

---
+++ intro/python 2/Tuples.py
@@ -0,0 +1,36 @@
+# instruments = ("clarinet", "piano", "drum", "violin")
+
+# print(instruments)
+
+
+# instruments = ("clarinet", "piano", "drum", "violin")
+
+# print(instruments[1])
+
+
+# instruments = ("clarinet", "piano", "drum", "violin")
+
+# print(instruments-[1])
+
+# instruments = ("clarinet", "piano", "drum", "violin", "guitar")
+
+# print(instruments[1:3])
+
+# instruments = ("clarinet", "piano", "drum", "violin")
+
+# if "piano" in instruments:
+#     print("The tuple contains the value of piano")
+# else:
+#     print("The tuple does not contains the value of piano")
+# grades = (95, 70, 85, 92, 100)
+
+# print(grades)
+# print(grades[-2])
+# print(grades[0:3])
+# friends = (3, 5, 7, 8, 10, 2, 50, 4)
+# guess = int(input("Pick a number"))
+
+# if guess in friends:
+#     print("Yes")
+# else:
+#     print("No")
