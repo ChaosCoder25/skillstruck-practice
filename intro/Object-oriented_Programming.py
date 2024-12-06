c37f8828de5b0d667e88fbd852148a89fb2cf550
ChaosCoder25 <mwill908@evit.edu>
2024-12-06 12:16:34 -0700

    exported from Skill Struck

---
+++ intro/Object-oriented_Programming.py
@@ -0,0 +1,20 @@
+# class Person:
+    
+#     def __init__(self, name, age, gender):
+#         self.name = name
+#         self.age = age
+#         self.gender = gender
+
+
+# class Dog:
+#   	def __init__(self, name, color, size):
+#           self.name =name
+#           self.color = color
+#           self.size = size
+
+# class Dessert:
+#     def __init__(self, shape, size, filling, topping):
+#         self.shape = shape 
+#         self.size = size
+#         self.filling = filling
+#         self.topping = topping
