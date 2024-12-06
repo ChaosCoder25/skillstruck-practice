de213f02733b4ce093dfff138c22bb6c19059cc3
ChaosCoder25 <mwill908@evit.edu>
2024-12-06 12:16:34 -0700

    exported from Skill Struck

---
+++ intro/python 3/creating_an_istance/object.py
@@ -0,0 +1,15 @@
+# class Person:
+#     def __init__(self, name, age, gender):
+#         self.name =name
+#         self.age = age
+#         self.gender = gender
+# p1 = Person("Jasmine", "15", "Female")
+# p2 =  Person("George", "67", "Male")
+# print("Person 1: " + p1.name + " " + p1.age + " " + p1.gender)
+# print("Person 2: " + p2.name + " " + p2.age + " " + p2.gender)
+class Hat:
+    def __init__(self, kind, color, material):
+        self.kind = kind
+        self.color = color
+        self.material = material
+myObject = Hat
