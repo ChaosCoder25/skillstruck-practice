c499859a31684fd76c31ce64484e0e90cb48b3f2
ChaosCoder25 <mwill908@evit.edu>
2024-12-06 12:16:35 -0700

    exported from Skill Struck

---
+++ intro/python its/python_math_library.py
@@ -0,0 +1,15 @@
+# import math
+# x = math.ceil(3.2)
+# print(x)
+# import math
+# x = math.floor(3.2)
+# print(x)
+# import math
+# x = math.pi
+# print(x)
+# import math
+# x = math.sqrt(100)
+# print(x)
+# import math
+# x = pow(2,4)
+# print(x)
