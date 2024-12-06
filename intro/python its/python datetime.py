d76e77efd944d118110186afee241767b509533c
ChaosCoder25 <mwill908@evit.edu>
2024-12-06 12:16:36 -0700

    exported from Skill Struck

---
+++ intro/python its/python datetime.py
@@ -0,0 +1,23 @@
+# import datetime
+# x = datetime.datetime.now()
+# print(x)
+
+# import datetime
+# today = datetime.date.today()
+# print(today)
+
+# import datetime
+# x = datetime.datetime.now()
+# print(x.strftime("%x"))
+
+# import datetime
+# x = datetime.datetime.now()
+# print(x.strftime("%m"))
+
+# import datetime
+# x = datetime.datetime.now()
+# print(x.strftime("%d"))
+
+# import datetime
+# x = datetime.datetime.now()
+# print(x.strftime("%y"))
