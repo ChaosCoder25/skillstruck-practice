24431d1232411ef7ac7068fc8fb6f27ff998dab1
ChaosCoder25 <mwill908@evit.edu>
2024-12-06 12:16:38 -0700

    exported from Skill Struck

---
+++ intro/python its/erro_handling.py
@@ -0,0 +1,36 @@
+# try:
+#     print(greeting)
+# except:
+#     print("A problem occurred")
+
+# greeting = "Hello"
+
+# try:
+#     print(greeting)
+# except:
+#     print("A problem occurred")
+# else:
+#     print("The code worked without a problem")
+
+
+# greeting = "Hello"
+
+# try:
+#     print(greeting)
+# except:
+#     print("A problem occurred")
+# else:
+#     print("The code worked without a problem")
+# finally:
+#     print("Your exception handling is complete")
+
+friend = "Taj"
+
+try:
+  print(brother)
+except:
+  print("A problem occurred") 
+else:
+  print("The code worked without a problem")
+finally:
+  print("Error handling is complete")
