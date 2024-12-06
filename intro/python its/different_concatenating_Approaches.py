34453cbcc83dc99914447f15fdedd8b553e25a12
ChaosCoder25 <mwill908@evit.edu>
2024-12-06 12:16:38 -0700

    exported from Skill Struck

---
+++ intro/python its/different_concatenating_Approaches.py
@@ -0,0 +1,20 @@
+# name = "James"
+# age = 16
+# grade = 10
+
+# print("Hello my name is " +  name + " and I am " + str(age) + " years old and am in grade " + str(grade))
+# name = "James"
+# age = 16
+# grade = 10
+
+# print(f"Hello my name is {name} and I am {age} years old and am in grade {grade}")
+
+# product = "shoes"
+# price = 10
+
+# print("Today we have", product, " on sale for only", price)
+
+
+
+
+
