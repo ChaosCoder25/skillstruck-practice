172715ade0a296f3522befadb94a7112b0e81a63
ChaosCoder25 <mwill908@evit.edu>
2024-12-06 12:16:13 -0700

    exported from Skill Struck

--- variables.py
+++ variables.py
@@ -1,6 +1,7 @@
-import pygame
-window = pygame.display.set_mode([400,300])
-pygame.display.set_caption("Images")
-image = pygame.image.load("imagel.png")
-windoe.blit(image, [0,0])
-pygame.display.flip()
+x = 5
+
+
+if x > 3:
+    print("Hello")
+else:
+    print("Hi")
